af026dd4f681df9cb25b8f9eb5973f314335dae1
ange <forever_youyou@foxmail.com>
2024-07-04 17:34:18 +0800

    🔧 build(build配置): vite build 配置
    vite build 相关配置

--- vite.config.ts
+++ vite.config.ts
@@ -30,5 +30,35 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  // 构建配置
+  build: {
+    outDir: 'dist', // 指定打包输出路径, 默认为 'dist'
+    assetsDir: 'static', // 静态资源输出路径(相对于 `outDir`), 默认为 'assets'
+    assetsInlineLimit: 4096, // 静态超过多少kb时, 生成单独文件
+    chunkSizeWarningLimit: 2000, // 单个chunk超过多少kb时, 发出警告
+    sourcemap: false, // 是否生成 sourcemap 文件
+    minify: 'esbuild', // 混淆器, 'terser' | 'esbuild', 默认 'esbuild', 设为 'terser' 下面的 terserOptions 才能生效
+    /* terserOptions: {
+      compress: {
+        drop_console: true, // 去除console
+        drop_debugger: true, // 去除debugger
+        keep_infinity: true // 防止 Infinity 被压缩成 1/0, 这可能会导致 Chrome 上的性能问题
+      },
+      format: {
+        comments: false // 去除注释
+      }
+    }, */
+    rollupOptions: {
+      output: {
+        entryFileNames: 'static/js/[name]-[hash].js',
+        chunkFileNames: 'static/js/[name]-[hash].js',
+        assetFileNames: 'static/[ext]/[name]-[hash].[ext]'
+      }
+    }
+  },
+  esbuild: {
+    drop: [/* 'console', */ 'debugger'], // 压缩时移除 console 和 debugger
+    pure: ['console.log', 'console.info'] // 压缩时移除特定的 console
   }
 })
