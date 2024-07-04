7e62fd1436ba438b75fd53e5177501f4dc9879be
ange <forever_youyou@foxmail.com>
2024-07-04 16:39:04 +0800

    ✨ feat(自动导入): 配置自动导入
    vue、vue-router、src/components 自动导入

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,6 +13,18 @@ export default defineConfig({
     vue(),
     vueJsx(),
     vueDevTools(),
+    AutoImport({
+      // 自动导入vue相关函数，如: ref、reactive、toRef等
+      imports: ['vue', 'vue-router'],
+      dts: 'src/auto-import.d.ts'
+    }),
+    Components({
+      // 指定组件位置，默认是 src/components 自动导入自定义组件
+      dirs: ['src/components'],
+      extensions: ['vue', 'tsx'],
+      // 配置文件生成位置
+      dts: 'src/components.d.ts'
+    })
   ],
   resolve: {
     alias: {
