fc92bc30c345fa743ac920cfee61825f61b7c071
ange <forever_youyou@foxmail.com>
2024-07-05 17:34:42 +0800

    ✨ feat(arco-design):
    新增依赖 @arco-design/webvue, sass, less

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import ArcoVue from '@arco-design/web-vue'
+import '@arco-design/web-vue/dist/arco.css'
+
 import App from './App.vue'
 import router from './router'
 
@@ -10,5 +13,6 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(ArcoVue)
 
 app.mount('#app')
