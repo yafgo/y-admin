9810b3ec93e6549b726660b98563cdaf59b25ca1
ange <forever_youyou@foxmail.com>
2024-07-08 16:11:54 +0800

    基础layout

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,41 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import AppLayout from '@/layout/AppLayout.vue'
 import HomeView from '../views/HomeView.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'root',
+    component: AppLayout,
+    children: [
+      {
+        path: '',
+        name: 'home',
+        component: HomeView
+      }
+    ]
+  },
+  {
+    path: '/about',
+    // name: 'about',
+    component: AppLayout,
+    // route level code-splitting
+    // this generates a separate chunk (About.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    // component: () => import('../views/AboutView.vue')
+    children: [
+      {
+        path: '',
+        name: 'about',
+        component: () => import('../views/AboutView.vue')
+      }
+    ]
+  }
+]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
+  routes: routes
 })
 
 export default router
