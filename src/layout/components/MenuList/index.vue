ef3fa517cb9679282960be3b55a549d53a13c1b2
ange <forever_youyou@foxmail.com>
2024-07-18 14:55:47 +0800

    菜单渲染

--- src/layout/components/MenuList/index.vue
+++ src/layout/components/MenuList/index.vue
@@ -1,7 +1,8 @@
 <script lang="tsx">
 import { compile } from 'vue'
-import type { RouteRecordRaw } from 'vue-router'
+import type { RouteLocationNormalized, RouteRecordRaw } from 'vue-router'
 import useMenuTree from './use-menu-tree'
+import { addRouteListener } from '@/utils/route-listener'
 
 export default defineComponent({
   name: 'MenuList',
@@ -9,12 +10,53 @@ export default defineComponent({
     const router = useRouter()
     const { menuTree } = useMenuTree()
 
+    /** 展开的菜单项key */
+    const openKeys = ref<string[]>([])
+    /** 选中的菜单项key */
+    const selectedKeys = ref<string[]>([])
+
     const goto = (el: RouteRecordRaw) => {
       router.push({
         name: el.name,
-        // path: el.path,
       })
     }
+
+    const findMenuOpenKeys = (target: string) => {
+      const result: string[] = []
+      let isFind = false
+      const backtrack = (item: RouteRecordRaw, keys: string[]) => {
+        if (item.name === target) {
+          isFind = true
+          result.push(...keys)
+          return
+        }
+        if (item.children?.length) {
+          item.children.forEach((el) => {
+            backtrack(el, [...keys, el.name as string])
+          })
+        }
+      }
+      menuTree.value.forEach((el: RouteRecordRaw) => {
+        if (isFind) return // Performance optimization
+        backtrack(el, [el.name as string])
+      })
+      return result
+    }
+
+    // 监听路由变化
+    addRouteListener((to: RouteLocationNormalized) => {
+      const { activeMenu, hideInMenu } = to.meta
+      if (hideInMenu && !activeMenu && !to.name) {
+        return
+      }
+
+      const menuOpenKeys = findMenuOpenKeys((activeMenu || to.name) as string)
+
+      const keySet = new Set([...menuOpenKeys, ...openKeys.value])
+      openKeys.value = [...keySet]
+
+      selectedKeys.value = [activeMenu || menuOpenKeys[menuOpenKeys.length - 1]]
+    }, true)
 
     // 渲染菜单项
     const renderMenu = () => {
@@ -61,6 +103,8 @@ export default defineComponent({
         mode="vertical"
         auto-open={false}
         level-indent={16}
+        v-model:open-keys={openKeys.value}
+        v-model:selected-keys={selectedKeys.value}
         onCollapse={(e: any) => console.log(e)}
       >
         {renderMenu()}
