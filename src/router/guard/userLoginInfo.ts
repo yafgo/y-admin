086ed12ad07995f196f5fdd575238da20bd51f42
ange <forever_youyou@foxmail.com>
2024-07-26 17:01:34 +0800

    路由守卫改进

--- src/router/guard/userLoginInfo.ts
+++ src/router/guard/userLoginInfo.ts
@@ -1,17 +1,16 @@
 import type { Router, LocationQueryRaw } from 'vue-router'
 
 import { useUserStore } from '@/stores'
+import { ROUTE_NAME_HOME } from '@/consts'
 
 export default function setupUserLoginInfoGuard(router: Router) {
   router.beforeEach(async (to, from, next) => {
     const userStore = useUserStore()
     if (userStore.isLogin) {
-      if (userStore.hasUserInfo) {
-        next()
-      } else {
+      // 没有获取用户信息, 先进行获取
+      if (!userStore.hasUserInfo) {
         try {
           await userStore.fetchUserInfo()
-          next()
         } catch (error) {
           await userStore.logout()
           next({
@@ -21,7 +20,15 @@ export default function setupUserLoginInfoGuard(router: Router) {
               ...to.query,
             } as LocationQueryRaw,
           })
+          return
         }
+      }
+
+      if (to.name === 'login') {
+        // 当前已登录, 不需要进登录页, 直接跳首页
+        next({ name: ROUTE_NAME_HOME })
+      } else {
+        next()
       }
     } else {
       if (to.name === 'login') {
