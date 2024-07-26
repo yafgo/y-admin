a939f1aede5c6e48f4325825c5bc1b340fee0401
ange <forever_youyou@foxmail.com>
2024-07-26 15:03:31 +0800

    右上角用户信息展示

--- src/layout/components/LayoutHeader/components/HeaderRightUser.vue
+++ src/layout/components/LayoutHeader/components/HeaderRightUser.vue
@@ -1,8 +1,13 @@
 <template>
   <a-dropdown trigger="hover" position="br">
-    <a-avatar :size="32" :style="{ marginRight: '8px', cursor: 'pointer' }">
-      <img alt="avatar" :src="avatar" />
-    </a-avatar>
+    <a-row align="center" :wrap="false" class="user-wrapper">
+      <a-avatar :size="32">
+        <img alt="avatar" :src="avatar" />
+      </a-avatar>
+      <span class="username">{{ userStore.name }}</span>
+      <icon-down></icon-down>
+    </a-row>
+
     <template #content>
       <a-doption>
         <a-space @click="todo">
@@ -34,10 +39,16 @@
 
 <script setup lang="ts">
 import { Message, Modal } from '@arco-design/web-vue'
+import { useUserStore } from '@/stores'
+import avatarDefault from '@/assets/logo.svg'
 
 defineOptions({ name: 'HeaderRightUser' })
 
-const avatar = ref('/src/assets/logo.svg')
+const userStore = useUserStore()
+
+const avatar = computed(() => {
+  return userStore.avatar || avatarDefault
+})
 
 const todo = () => {
   Message.success('todo')
@@ -63,6 +74,27 @@ const handleLogout = () => {
 </script>
 
 <style lang="scss" scoped>
+.user-wrapper {
+  cursor: pointer;
+  color: var(--color-text-1);
+
+  .username {
+    margin-left: 10px;
+    white-space: nowrap;
+  }
+
+  .arco-icon-down {
+    transition: all 0.3s;
+    margin-left: 2px;
+  }
+
+  &.arco-dropdown-open {
+    .arco-icon-down {
+      transform: rotate(180deg);
+    }
+  }
+}
+
 .menu-item-logout {
   color: rgb(var(--red-6));
 }
